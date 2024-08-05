444b2b5d188b1e078f31d74660dd1dcc9f409d37
bhagyalaxmi25 <palaibhagyalaxmi81@gmail.com>
2024-08-05 21:03:27 +0530

    Php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    echo"<h1>I like Pizza</h1>";
+    echo"<h3>It's really good</h3>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <button>Order pizza</button>
+    
+</body>
+</html>
